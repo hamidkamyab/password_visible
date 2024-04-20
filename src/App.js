e36c4606f2f2ab5dc34a3ea60ff5a0693be3e1e2
Hamid Kamyab <mr_kamyab@yahoo.com>
2024-04-20 23:47:52 +0330

    Showing and not showing the password

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import * as Vsc from "react-icons/vsc";
 
 function App() {
+
+  const [inputType,setInputType]=useState('password');
+  const [iconEye,setIconEye]=useState('hidden');
+  // VscEye
+  // VscEyeClosed
+  const handleClick =()=>{
+    if(inputType == 'password'){
+      setInputType('text')
+      setIconEye('show')
+    }else{
+      setInputType('password')
+      setIconEye('hidden')
+    }
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App w-100 vh-100 d-flex justify-content-center align-items-center">
+        <div className="input-fields bg-white p-2 d-flex gap-2 rounded-5">
+          <span onClick={handleClick} className="fs-5" role="button"  >
+            {iconEye == 'show'? <Vsc.VscEye /> : <Vsc.VscEyeClosed /> }
+          </span>
+          <input type={inputType} className="ltr" />
+        </div>
     </div>
   );
 }
