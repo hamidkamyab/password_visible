fe502f356a6c9c7dc3cd1f8c6feebccda20c285d
Hamid Kamyab <mr_kamyab@yahoo.com>
2024-04-21 21:52:36 +0330

    Partial project update

--- src/App.js
+++ src/App.js
@@ -4,16 +4,12 @@ import * as Vsc from "react-icons/vsc";
 function App() {
 
   const [inputType,setInputType]=useState('password');
-  const [iconEye,setIconEye]=useState('hidden');
-  // VscEye
-  // VscEyeClosed
+
   const handleClick =()=>{
     if(inputType == 'password'){
       setInputType('text')
-      setIconEye('show')
     }else{
       setInputType('password')
-      setIconEye('hidden')
     }
     
   }
@@ -22,7 +18,7 @@ function App() {
     <div className="App w-100 vh-100 d-flex justify-content-center align-items-center">
         <div className="input-fields bg-white p-2 d-flex gap-2 rounded-5">
           <span onClick={handleClick} className="fs-5" role="button"  >
-            {iconEye == 'show'? <Vsc.VscEye /> : <Vsc.VscEyeClosed /> }
+            {inputType == 'text'? <Vsc.VscEye /> : <Vsc.VscEyeClosed /> }
           </span>
           <input type={inputType} className="ltr" />
         </div>
